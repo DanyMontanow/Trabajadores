d7f9c31801ff764710c988bd5e4e327035810aca
salma <danielamontano2102@gmail.com>
2016-10-13 19:13:31 -0700

    Tarea

---
+++ Trabajadores/Trabajadores/Persona.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabajadores
+{
+    class Persona
+    {
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+        public int Salario { get; set; }
+        public double Salario2 { get; set; }
+
+    }
+}
---
+++ Trabajadores/Trabajadores/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Trabajadores 
+{
+    class Program : Persona
+    {
+      public void Datos()
+        {
+            Console.WriteLine("Ingrese Nombre:");
+            Nombre = Console.ReadLine();
+            string linea;
+            Console.WriteLine("Ingrese Edad:");
+            linea = Console.ReadLine();
+            Edad = int.Parse(linea);
+            Console.WriteLine("Ingrese Salario:");
+            linea = Console.ReadLine();
+            Salario = int.Parse(linea);
+        }
+        public void Muestra()
+        {
+            Console.Write("Nombre:");
+            Console.WriteLine(Nombre);
+            Console.Write("Salario Total:");
+            Console.WriteLine(Salario2);
+        }
+         public void Compara()
+        {
+            if (Salario > 10000)
+            {
+                Salario2 = (Salario*0.20) -Salario;
+            }
+            else
+            {
+                Salario2 = (Salario * 0.16) - Salario;
+            }
+            Console.ReadKey();
+        }
+  
+        static void Main(string[] args)
+        {
+            Persona Per1 = new Persona();
+            
+
+        }
+    }
+}
